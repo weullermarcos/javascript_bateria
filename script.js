0c109952ba571059359d1495589bfb34e896c696
weullermarcos <weullermarcos@hotmail.com>
2021-07-21 17:48:37 -0300

    Tocando ao clicar no botão

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ document.body.addEventListener('keyup', (event)=>{
     playSound(event.code.toLocaleLowerCase());
 });
 
-function playSound(sound){
+document.querySelector('.composer button').addEventListener('click', () =>{
 
-    //alert('chegou aqui');
+    let song = document.querySelector('#input').value;
+
+    if(song !== ''){
+
+        //gerando um array com cada item da minha string
+        let songArray = song.split('');
+
+        // console.log(songArray);
+
+        //toca a composição
+        playComposition(songArray);
+    }
+
+});
+
+function playSound(sound){
 
     //verifica qual tecla foi recebida
     let audioElement = document.querySelector(`#s_${sound}`);
@@ -14,10 +29,11 @@ function playSound(sound){
 
     //se achar o elemento, toca
     if(audioElement){
-        audioElement.currenTime = 0;
+        audioElement.currentTime = 0;
         audioElement.play();
     }
 
+    //se achar o elemento, ativa o efeito da div
     if(keyElement){
 
         //adicionando efeito de ativo
@@ -29,5 +45,24 @@ function playSound(sound){
         }, 300);
     }
 
+}
+
+function playComposition(songArray){
+
+    //define intervalo entre as notas
+    let wait = 0;
+
+    for(let songItem of songArray){
+
+        setTimeout(()=>{
+
+            // chama a função para tocar
+            playSound(`key${songItem}`);
+
+        }, wait);
+
+        //incrementando o tempo de timeout
+        wait += 250;
+    }
 }
 
